91431729d2ed1b9ecd688ee5f7e534548500d4b6
kirillkrohmal <krohmal_kirill@mail.ru>
2020-04-04 16:08:08 +0300

    Сервис - Кинотеатр[#234380]

--- src/main/java/ru/job4j/controller/AddServlet.java
+++ src/main/java/ru/job4j/controller/AddServlet.java
@@ -15,6 +15,7 @@ public class AddServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("/add.jsp");
+        req.setAttribute("list", UserStorage.getInstance().getUserList());
         dispatcher.forward(req, resp);
     }
 
